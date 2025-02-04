7b2826ba43e783b25d05b22786055081233e9562
jving <j.vingback@gmail.com>
2025-02-04 21:37:57 +0100

    Removed some lint

--- pyslides.py
+++ pyslides.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
+import os
+import sys
+import re
+import math
+from PIL import Image
+import argparse
 import pygame
+from pygame.locals import QUIT, KEYDOWN, K_ESCAPE, MOUSEBUTTONDOWN, K_RIGHT, K_LEFT
 try:
     import pygame.gfxdraw
     NO3D = False
 except:
     NO3D = True
-import os
-import sys
-import re
-import math
-from pygame.locals import *
-from PIL import Image
 from pygame_markdown import MarkdownRenderer
-import argparse
+
 
 # Standard values. If settings is supplied, those present will be overwritten.
 CONFIG = {
@@ -39,15 +40,15 @@ def load_markdown(filename):
 def parse_slides(md_text, basedir="./"):
     slides = []
     current_slide = {"title": "", "content": [], "images": [], "transition": "slide"}
-    
+
     for line in md_text.split("\n"):
         if line.startswith("# "):  # New slide
             if current_slide["title"]:
                 slides.append(current_slide)
             current_slide = {"title": line[2:], "content": [], "images": [], "transition": "slide"}
-        elif re.match(r"<!--\s*transition:\s*(slide|fade|3d-rotate|3d-zoom)\s*-->", line):  
+        elif re.match(r"<!--\s*transition:\s*(slide|fade|3d-rotate|3d-zoom)\s*-->", line):
             current_slide["transition"] = line.split(":")[1].strip().replace("-->", "").strip()
-        elif line.startswith("!"):  
+        elif line.startswith("!"):
             start = line.find("(") + 1
             end = line.find(")")
             if start > 0 and end > start:
@@ -60,28 +61,32 @@ def parse_slides(md_text, basedir="./"):
 
     return slides
 
-# Load and scale image
+
 def load_image(path):
+    """ Loads and scales an image """
     if not os.path.exists(path):
         return None
     img = Image.open(path)
     img.thumbnail((CONFIG['IMAGE_MAX']['WIDTH'], CONFIG['IMAGE_MAX']['HEIGHT']))
     return pygame.image.fromstring(img.tobytes(), img.size, img.mode)
 
-# Slide transition
+
 def slide_transition(screen, old_slide, new_slide, direction, clock):
+    """Slides from slide to slide """
     steps = int(CONFIG['FPS']*CONFIG['TRANSITION_DURATION'])
     w = CONFIG['SCREEN']['WIDTH']
     offset = w if direction == "right" else -w
     for i in range(0, w, int(w/steps)):
         screen.fill(CONFIG['BORDER']['COLOR'])
         screen.blit(old_slide, (i if direction == "left" else -i, CONFIG['BORDER']['WIDTH']))
-        screen.blit(new_slide, (offset - i if direction == "right" else offset + i, CONFIG['BORDER']['WIDTH']))
+        screen.blit(new_slide, (offset - i if direction == "right" else offset + i,
+                                CONFIG['BORDER']['WIDTH']))
         pygame.display.flip()
         clock.tick(CONFIG['FPS'])
 
-# Fade transition
+
 def fade_transition(screen, old_slide, new_slide, clock):
+    """ Fades between two slides """
     steps = int(CONFIG['FPS']*CONFIG['TRANSITION_DURATION'])
     for alpha in range(0, 256, int(255 / steps)):
         screen.fill(CONFIG['BORDER']['COLOR'])
@@ -93,24 +98,25 @@ def fade_transition(screen, old_slide, new_slide, clock):
         clock.tick(CONFIG['FPS'])
 
 
-# 2D Rotate transition
 def rotate_2d_transition(screen, old_slide, new_slide, clock):
     """ Flat version since pygame has no skew transform """
+    width = CONFIG['SCREEN']['WIDTH']-CONFIG['BORDER']['WIDTH']
+    height = CONFIG['SCREEN']['HEIGHT']-CONFIG['BORDER']['WIDTH']
     steps = int(CONFIG['FPS']*CONFIG['TRANSITION_DURATION'])
     for angle in range(0, 181, int(180 / steps)):
         screen.fill(CONFIG['BORDER']['COLOR'])
         if angle <= 90:
-            rotated_surface = pygame.transform.scale(old_slide, (int(WIDTH * math.cos(math.radians(angle))), HEIGHT))
-            screen.blit(rotated_surface, ((WIDTH - rotated_surface.get_width()) // 2, CONFIG['BORDER']['WIDTH']))
+            rotated_surface = pygame.transform.scale(old_slide, (int(width * math.cos(math.radians(angle))), height))
+            screen.blit(rotated_surface, ((width - rotated_surface.get_width()) // 2, CONFIG['BORDER']['WIDTH']))
         else:
             rotated_surface = pygame.transform.scale(new_slide, (int(WIDTH * math.cos(math.radians(180 - angle))), HEIGHT))
-            screen.blit(rotated_surface, ((WIDTH - rotated_surface.get_width()) // 2, CONFIG['BORDER']['WIDTH']))
+            screen.blit(rotated_surface, ((width - rotated_surface.get_width()) // 2, CONFIG['BORDER']['WIDTH']))
         pygame.display.flip()
         clock.tick(CONFIG['FPS'])
 
 
-# 3D Rotate transition
 def rotate_3d_transition(screen, old_slide, new_slide, clock):
+    """ Makes a pseudo-3D rotation. Could be better if we could skrew the slide """
     steps = int(CONFIG['FPS']*CONFIG['TRANSITION_DURATION'])
     w = CONFIG['SCREEN']['WIDTH']-CONFIG['BORDER']['WIDTH']
     h = CONFIG['SCREEN']['HEIGHT']-CONFIG['BORDER']['WIDTH']
@@ -144,8 +150,9 @@ def rotate_3d_transition(screen, old_slide, new_slide, clock):
         pygame.display.flip()
         clock.tick(CONFIG['FPS'])
 
-# 3D Zoom transition
+
 def zoom_3d_transition(screen, old_slide, new_slide, clock):
+    """ Zooms out of old slide and in to new """
     steps = int(CONFIG['FPS']*CONFIG['TRANSITION_DURATION'])/2
     W = CONFIG['SCREEN']['WIDTH']-CONFIG['BORDER']['WIDTH']*2
     H = CONFIG['SCREEN']['HEIGHT']-CONFIG['BORDER']['WIDTH']*2
@@ -154,20 +161,23 @@ def zoom_3d_transition(screen, old_slide, new_slide, clock):
     for scale in range(100, 0, -int(100 / steps)):
         screen.fill(CONFIG['BORDER']['COLOR'])
         zoomed_surface = pygame.transform.scale(old_slide, (int(W * scale / 100), int(H * scale / 100)))
-        screen.blit(zoomed_surface, ((W - zoomed_surface.get_width()) // 2 + B, (H - zoomed_surface.get_height()) // 2 + B))
+        screen.blit(zoomed_surface, ((W - zoomed_surface.get_width()) // 2 + B,
+                                     (H - zoomed_surface.get_height()) // 2 + B))
         pygame.display.flip()
         clock.tick(CONFIG['FPS'])
 
     for scale in range(0, 100, int(100 / steps)):
         screen.fill(CONFIG['BORDER']['COLOR'])
         zoomed_surface = pygame.transform.scale(new_slide, (int(W * scale / 100), int(H * scale / 100)))
-        screen.blit(zoomed_surface, ((W - zoomed_surface.get_width()) // 2 + B, (H - zoomed_surface.get_height()) // 2 + B))
+        screen.blit(zoomed_surface, ((W - zoomed_surface.get_width()) // 2 + B,
+                                     (H - zoomed_surface.get_height()) // 2 + B))
         pygame.display.flip()
         clock.tick(CONFIG['FPS'])
 
 
 # Render slide content to a surface
 def render_slide(slide):
+    """ Pre-render a slide """
     W = CONFIG['SCREEN']['WIDTH']-CONFIG['BORDER']['WIDTH']*2
     H = CONFIG['SCREEN']['HEIGHT']-CONFIG['BORDER']['WIDTH']*2
     surface = pygame.Surface((W,H))
@@ -176,8 +186,7 @@ def render_slide(slide):
 
     md = MarkdownRenderer()
     mdtxt = "# " + slide['title'] + "\n" + "\n".join(slide['content'])
-    #print(mdtxt)
-    
+
     md.set_markdown_from_string(md_string=mdtxt)
     md.set_area(surface=surface, offset_x=0, offset_y=0, width=W//2, height=H)
     md.set_color_background(*CONFIG['BG_COLOR'])
@@ -195,14 +204,15 @@ def render_slide(slide):
 
     return surface
 
-# Main function
 def run_slideshow(md_file):
+    """ The main function to run the presentation """
     basedir = "./"
     if "/" in md_file:
         basedir = "/".join(md_file.split('/')[:-1]) + "/"
     pygame.init()
     if CONFIG['SCREEN']['FULLSCREEN']:
-      screen = pygame.display.set_mode((CONFIG['SCREEN']['WIDTH'], CONFIG['SCREEN']['HEIGHT']), pygame.FULLSCREEN)
+      screen = pygame.display.set_mode((CONFIG['SCREEN']['WIDTH'], CONFIG['SCREEN']['HEIGHT']),
+                                       pygame.FULLSCREEN)
     else:
       screen = pygame.display.set_mode((CONFIG['SCREEN']['WIDTH'], CONFIG['SCREEN']['HEIGHT']))
     pygame.display.set_caption("Markdown Slideshow")
@@ -277,14 +287,13 @@ if __name__ == "__main__":
     parser.add_argument('slides_filename')
     res = parser.parse_args(sys.argv[1:])
 
+    newstyle = False
     if res.style_file:
         import json
         try:
             newstyle = json.load(open(res.style_file,'r'))
         except:
             print("Failed to open style. using standard values")
-            newstyle = False
-
         if newstyle:
             for key in newstyle.keys():
                 if type(newstyle[key]) == dict:
